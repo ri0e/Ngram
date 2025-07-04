8bde9f9d09ef6faab8148a55e868b59512d83a21
GlassyBridge <Leul2027@hmacademy.org>
2025-07-04 15:37:06 +0300

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import ngram
-
-word_count = 10
-_ngram = ngram.read_from_json('N-gram_2025_06_29_17_12_56.json')
-text = 'I'
-next_word = ngram.predict_more(_ngram, text, word_count)
-
-print(next_word)
